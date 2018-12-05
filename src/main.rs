e4f77b2cd6fd16b44ccf054ef18d2f0d91829401
Benjamin Garfield <ben@clypd.com>
2018-12-04 22:46:41 -0500

    Find the final value from summing the given list
    List is all "+int" or "-int", separated by newlines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::fs;
+use std::str;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let filename = &args[1];
+
+    println!("Using file: {}", filename);
+
+    let res = fs::read_to_string(filename)
+        .expect("something went horribly wrong")
+        .as_mut_str()
+        .split_whitespace()
+        .fold(0, |acc, next_val| input_iter(acc, next_val));
+
+    println!("solution is: {}", res);
+}
+
+fn input_iter(acc: isize, next_val: &str) -> isize {
+    let nv: isize = next_val.to_string().parse().unwrap();
+
+    return acc + nv;
 }
