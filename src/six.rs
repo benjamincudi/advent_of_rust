4435982720316516869c7c38becbb61b75151e3f
Benjamin Garfield <ben@clypd.com>
2018-12-08 17:41:07 -0500

    Make seconary struct to allow for IDs to be set, track interesting info

--- src/six.rs
+++ src/six.rs
@@ -3,9 +3,15 @@ use std::error;
 use std::fmt;
 use std::str::FromStr;
 
+struct RawPoint {
+    x_offset: usize,
+    y_offset: usize,
+}
+
 struct Point {
     x_offset: usize,
     y_offset: usize,
+    id: usize,
 }
 
 impl Point {
@@ -40,12 +46,12 @@ impl error::Error for PointParseError {
     }
 }
 
-impl FromStr for Point {
+impl FromStr for RawPoint {
     type Err = PointParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let data_chunks: Vec<usize> = s.split(", ").map(|s| s.parse().unwrap()).collect();
-        let p = Point {
+        let p = RawPoint {
             x_offset: data_chunks[0],
             y_offset: data_chunks[1],
         };
@@ -54,12 +60,31 @@ impl FromStr for Point {
 }
 
 pub fn part_one(file_contents: &String) -> () {
+    let mut x_max: usize = 0;
+    let mut y_max: usize = 0;
+    let mut count: usize = 0;
     let input_points: Vec<Point> = file_contents
         .clone()
         .as_mut_str()
         .split("\n")
-        .map(|p_str| Point::from_str(p_str).unwrap())
-        .collect();
+        .map(|p_str| {
+            let rp = RawPoint::from_str(p_str).unwrap();
+            count += 1;
+            let p = Point {
+                x_offset: rp.x_offset,
+                y_offset: rp.y_offset,
+                id: count,
+            };
+            x_max = match x_max.cmp(&p.x_offset) {
+                Ordering::Less => p.x_offset,
+                _ => x_max,
+            };
+            y_max = match y_max.cmp(&p.y_offset) {
+                Ordering::Less => p.y_offset,
+                _ => y_max,
+            };
+            return p;
+        }).collect();
 
     println!("found {} points", input_points.len());
 }
