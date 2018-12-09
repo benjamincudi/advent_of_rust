8959b342d91bcf59b3c482cbf5d0d4d68bf65feb
Benjamin Garfield <ben@clypd.com>
2018-12-09 18:20:16 -0500

    Set up recursive parsing for Nodes
    The input is in a recursive format, so parse it in that manner to avoid
    headaches

--- src/eight.rs
+++ src/eight.rs
@@ -1,6 +1,7 @@
 use std::error;
 use std::fmt;
 
+#[derive(Clone)]
 struct Header {
     num_children: usize,
     metadata_count: usize,
@@ -37,6 +38,36 @@ impl Node {
 
     fn append_metadata(&mut self, m: usize) {
         self.metadata.push(m);
+    }
+
+    fn from_vec_usize(i: &mut Vec<usize>) -> Result<Self, NodeParseError> {
+        let self_header_data = vec![i.remove(0), i.remove(0)];
+
+        let header = Header {
+            num_children: self_header_data[0],
+            metadata_count: self_header_data[1],
+        };
+
+        let mut node = Node {
+            header: header.clone(),
+            children: vec![],
+            metadata: vec![],
+        };
+
+        let mut remaining_children = header.num_children.clone();
+        while &remaining_children > &0 {
+            match Node::from_vec_usize(i) {
+                Ok(n) => node.append_child(n),
+                Err(_) => panic!("failed to parse"),
+            };
+            remaining_children -= 1;
+        }
+
+        while node.metadata.len() < node.header.metadata_count {
+            node.append_metadata(i.remove(0));
+        }
+
+        return Ok(node);
     }
 }
 
