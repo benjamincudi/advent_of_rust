6369c89c2b84f36207f2a2c6fb3410a7456a0bbd
Benjamin Garfield <ben@clypd.com>
2018-12-08 20:08:51 -0500

    Create a HashMap of step to blocking steps
    We don't actually need the tuples since there is no meaningful order
    that they imply. Instead, we'll be able to repeatedly walk the HashMap
    and look for the steps that are ready to be completed

--- src/seven.rs
+++ src/seven.rs
@@ -1,19 +1,24 @@
+use std::collections::HashMap;
+
 pub fn part_one(file_contents: &String) -> () {
     let mut num_directions: usize = 0;
 
-    let direction_tuples: Vec<(String, String)> = file_contents
+    let mut step_to_blockers: HashMap<String, Vec<String>> = HashMap::new();
+
+    file_contents
         .clone()
         .as_mut_str()
         .split("\n")
-        .map(|d_str| {
+        .for_each(|d_str| {
             let lower_step = d_str.to_lowercase();
             num_directions += 1;
             let steps: Vec<&str> = lower_step.split("step ").collect();
             let blocker = steps[1][0..1].to_string();
             let blockee = steps[2][0..1].to_string();
 
-            return (blocker, blockee);
-        }).collect();
+            let v = step_to_blockers.entry(blockee).or_insert(vec![]);
+            v.push(blocker);
+        });
 
     println!("{} directions found", num_directions);
 }
