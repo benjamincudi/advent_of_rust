8d808bfe796e0aa622448625ae1cde12a9082085
Benjamin Garfield <ben@clypd.com>
2018-12-09 20:01:16 -0500

    Play the insane marble game
    What the hell are these elves on
    
    There's some maybe-shenanigans to protect against a too-small board
    needing to cycle "back" 7 spaces, but my local napkin says that the
    math should be right there

--- src/nine.rs
+++ src/nine.rs
@@ -1,3 +1,6 @@
+use std::cmp::Ordering;
+use std::collections::HashMap;
+
 pub fn part_one(file_contents: &String) -> () {
     let data_chunks: Vec<usize> = file_contents
         .clone()
@@ -13,5 +16,59 @@ pub fn part_one(file_contents: &String) -> () {
     let players: usize = data_chunks[0];
     let final_marble_value: usize = data_chunks[1];
 
+    let mut board: Vec<usize> = vec![0];
+
+    let mut scoreboard: HashMap<usize, usize> = HashMap::new();
+
+    let mut current_index: usize = 0;
+    let mut current_player: usize = 0;
+    for n in 1..final_marble_value {
+        let board_len = board.len();
+        if n % 23 == 0 {
+            let player_score = scoreboard.entry(current_player).or_insert(0);
+            *player_score += n;
+            current_index = match current_index.cmp(&7) {
+                Ordering::Equal => 0,
+                Ordering::Greater => current_index - 7,
+                Ordering::Less => {
+                    let c_i = if board_len > 7 {
+                        board_len - 7 + current_index
+                    } else {
+                        // Here be dragons
+                        println!("math shenanigans to find new index, cycling on a small board");
+                        let net = 7 % board_len;
+                        board_len + current_index - net
+                    };
+                    c_i
+                }
+            };
+            let secondary_scored_marble = board.remove(current_index);
+            *player_score += secondary_scored_marble
+        } else {
+            current_index = match (current_index + 2).cmp(&board_len) {
+                Ordering::Less => {
+                    board.insert(current_index + 2, n);
+                    current_index + 2
+                }
+                Ordering::Equal => {
+                    board.push(n);
+                    board.len() - 1
+                }
+                Ordering::Greater => {
+                    board.insert(1, n);
+                    1
+                }
+            };
+        }
+
+        current_player = (current_player + 1) % players;
+    }
+
+    let (_, top_score) = scoreboard
+        .into_iter()
+        .max_by(|(_, a), (_, b)| a.cmp(b))
+        .unwrap();
+
     println!("{} players, {} marbles", players, final_marble_value + 1);
+    println!("high score was {}", top_score);
 }
