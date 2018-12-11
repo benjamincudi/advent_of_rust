3712037a6365ee20359ac42f568b91ce81e2add2
Benjamin Garfield <ben@clypd.com>
2018-12-10 22:31:11 -0500

    Solution for part one - print the message out
    Lots of comments along the way, since the narrowing approach was a hedge
    that panned out. This leverages some somewhat meta knowledge that the
    solution would likely have an (at least roughly) uniform height and
    probably a uniform baseline.
    The inputs were likely generated by doing this problem in reverse: given
    ascii text, apply a velocity to each forming character and find out
    where they all end up. Then reverse the velocity and provide that as
    input for us all
    
    Without a guarantee of this nature, crafting a solution would get much
    more complicated and likely require human intervention

--- src/ten.rs
+++ src/ten.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::error;
 use std::fmt;
 use std::isize;
@@ -63,5 +65,91 @@ impl FromStr for RawPoint {
             y_vel: data_chunks[3],
         };
         return Ok(p);
+    }
+}
+
+pub fn part_one(file_contents: &String) -> () {
+    let points = file_contents
+        .clone()
+        .as_mut_str()
+        .split("\n")
+        .map(|s| s.parse().unwrap())
+        .collect::<Vec<RawPoint>>();
+
+    // Since the points must be converging, compare vertical spread against MAX
+    let mut min_spread: isize = isize::MAX;
+
+    // These update every loop - their initial value is only to appease the compiler
+    let mut min_spread_time: isize = 0;
+    let mut y_max: isize = 0;
+    let mut y_min: isize = 0;
+    for t in 1.. {
+        // For each moment in time, determine the current vertical spread
+        let mut y_max_loop: isize = isize::MIN;
+        let mut y_min_loop: isize = isize::MAX;
+        for p in points.clone().into_iter() {
+            let current_y: isize = p.y_init + (p.y_vel * t);
+            match current_y.cmp(&y_max_loop) {
+                Ordering::Greater => y_max_loop = current_y,
+                _ => (),
+            }
+            match current_y.cmp(&y_min_loop) {
+                Ordering::Less => y_min_loop = current_y,
+                _ => (),
+            }
+        }
+        let spread = y_max_loop - y_min_loop;
+        match min_spread.cmp(&spread) {
+            Ordering::Greater => min_spread = spread,
+            // As soon as we're diverging, we know the previous second was the closes things get
+            _ => break,
+        }
+        // Since these only update if we're still converging, these represent the values we're interested in
+        y_max = y_max_loop;
+        y_min = y_min_loop;
+        min_spread_time = t;
+    }
+    // Once we have the max and min Y values, find the same for X at the solution tim
+    let x_min_point: RawPoint = points
+        .clone()
+        .into_iter()
+        .min_by(|a, b| {
+            (a.x_init + (a.x_vel * min_spread_time)).cmp(&(b.x_init + (b.x_vel * min_spread_time)))
+        }).unwrap();
+    let x_min: isize = x_min_point.x_init + (x_min_point.x_vel * min_spread_time);
+    let x_max_point: RawPoint = points
+        .clone()
+        .into_iter()
+        .max_by(|a, b| {
+            (a.x_init + (a.x_vel * min_spread_time)).cmp(&(b.x_init + (b.x_vel * min_spread_time)))
+        }).unwrap();
+    let x_max: isize = x_max_point.x_init + (x_max_point.x_vel * min_spread_time);
+
+    // To make it easier to print the solution letters,
+    // Make a HashMap<y_value, <x_value, should_print>>
+    let mut grid: HashMap<isize, HashMap<isize, bool>> = HashMap::new();
+    for p in points.clone().into_iter() {
+        // Calculate the current value for each point
+        // and then shift everything towards the origin
+        let y: isize = p.y_init + (p.y_vel * min_spread_time) - y_min;
+        let x: isize = p.x_init + (p.x_vel * min_spread_time) - x_min;
+
+        let row = grid.entry(y).or_insert(HashMap::new());
+        row.insert(x, true);
+    }
+    // For each row
+    for y in 0..y_max - y_min + 1 {
+        let mut row_str: String = "".to_string();
+        let row = grid.entry(y).or_default();
+        // Find every point in that row and render a # for it
+        for x in 0..x_max - x_min + 1 {
+            match row.get(&x) {
+                Some(_) => row_str += "#",
+                None => row_str += " ",
+            }
+        }
+
+        // Print each row as we complete it
+        println!("{}", row_str);
     }
 }
